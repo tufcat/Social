55e976a230bdb2a4524720476a756a172fbddc6a
Brad McManus <mcmanus.brad@gmail.com>
2011-09-17 01:15:41 -0400

    First commit: Skeleton project

---
+++ src/com/hypnotoad/Feed.java
@@ -0,0 +1,13 @@
+package com.hypnotoad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Feed extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
